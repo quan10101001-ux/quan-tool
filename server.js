420b53e67b488de75d2c98cd9468f0522c7a18bd
quan10101001-ux <quan10101001@gmail.com>
2026-05-25 21:06:22 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,30 +3,56 @@ const net = require('net');
 const TUNNEL_PORT = 7000;
 const PUBLIC_PORT = 6379;
 
-const clients = [];
-let tunnelSocket = null;
+const tunnelQueue = [];
+const clientQueue = [];
+
+function tryPair() {
+  while (tunnelQueue.length && clientQueue.length) {
+    const tunnel = tunnelQueue.shift();
+    const client = clientQueue.shift();
+
+    tunnel.pipe(client);
+    client.pipe(tunnel);
+
+    tunnel.on('error', () => client.destroy());
+    client.on('error', () => tunnel.destroy());
+    tunnel.on('close', () => client.destroy());
+    client.on('close', () => tunnel.destroy());
+
+    console.log('[pair] Paired tunnel with client');
+  }
+}
 
 net.createServer(sock => {
-  tunnelSocket = sock;
-  console.log('[tunnel] Local machine connected');
+  console.log('[tunnel] New tunnel slot ready');
+  tunnelQueue.push(sock);
 
-  sock.on('error', err => console.log('[tunnel] error:', err.message));
   sock.on('close', () => {
-    tunnelSocket = null;
-    console.log('[tunnel] disconnected');
+    const i = tunnelQueue.indexOf(sock);
+    if (i !== -1) tunnelQueue.splice(i, 1);
   });
+
+  sock.on('error', () => {
+    const i = tunnelQueue.indexOf(sock);
+    if (i !== -1) tunnelQueue.splice(i, 1);
+  });
+
+  tryPair();
 }).listen(TUNNEL_PORT, () => console.log(`[tunnel] Waiting on port ${TUNNEL_PORT}`));
 
 net.createServer(sock => {
-  if (!tunnelSocket) {
-    console.log('[public] No tunnel, dropping connection');
-    return sock.destroy();
-  }
+  console.log('[public] Client connected');
+  clientQueue.push(sock);
 
-  console.log('[public] Client connected, piping to tunnel');
-  sock.pipe(tunnelSocket);
-  tunnelSocket.pipe(sock);
+  sock.on('close', () => {
+    const i = clientQueue.indexOf(sock);
+    if (i !== -1) clientQueue.splice(i, 1);
+  });
 
-  sock.on('error', () => sock.destroy());
-  sock.on('close', () => tunnelSocket && tunnelSocket.unpipe());
+  sock.on('error', () => {
+    const i = clientQueue.indexOf(sock);
+    if (i !== -1) clientQueue.splice(i, 1);
+  });
+
+  tryPair();
 }).listen(PUBLIC_PORT, () => console.log(`[public] Redis available on port ${PUBLIC_PORT}`));
